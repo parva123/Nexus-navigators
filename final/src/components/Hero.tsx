ae58d83edbdb96a1bce8541a1d938545f3cccc63
parva123 <parva.mehta99@gmail.com>
2025-03-15 17:20:02 -0400

    hi

--- final/src/components/Hero.tsx
+++ final/src/components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
       </div>
 
       <div className="container mx-auto px-4 md:px-6">
-        <div className="max-w-6xl mx-auto text-center">
+        <div className="max-w-4xl mx-auto text-center">
           {/* Eyebrow */}
           
 
